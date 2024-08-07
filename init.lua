927362aa11a42d143820df9b429033b8374c13df
mahedikd <mahedi66958@gmail.com>
2024-08-07 10:39:18 +0000

    misc

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+-- Set encoding to UTF-8
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
