75f5302b2e423c2fe9736a0026c60f695f9b4232
mahedikd <mahedi66958@gmail.com>
2024-08-10 06:47:10 +0000

    highlight on yank group removed

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank { higroup = "IncSearch", timeout = 250 }
+    vim.highlight.on_yank { timeout = 250 }
   end,
 })
 
